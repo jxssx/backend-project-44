7f1a0d337e61b5c0abd29494735bca19b4c3f30d
Danil <danil.druzhina@gmail.com>
2022-10-14 15:26:40 +0300

    added greeting to brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import readLineSync from "readline-sync";
+
 console.log('Welcome to the Brain Games!');
+const name = readLineSync.question('May i have your name? ');
+console.log('Hello, ' + name + '!');
