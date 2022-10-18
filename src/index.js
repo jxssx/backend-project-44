1275f29828f24570a1cc67929ffc014819731b85
jxssx <danil.druzhina@gmail.com>
2022-10-18 15:22:25 +0300

    Fix src/index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import { greeting, user } from './cli.js';
 
 let lost = false;
 
-const game = (desc, generateQuestion, expectedAnswer) => {
+const game = (desc, generateQuestion, findCorrectAnswer) => {
   greeting();
   console.log(desc);
   for (let i = 0; i < 3; i += 1) {
     console.log('Question:', generateQuestion());
     const answer = readLineSync.question('Your answer: ');
-    if (answer === expectedAnswer().toString()) {
+    if (answer === findCorrectAnswer().toString()) {
       console.log('Correct!');
     } else {
       lost = true;
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer()}'. \nLet's try again, ${user.name}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${findCorrectAnswer()}'. \nLet's try again, ${user.name}!`);
       break;
     }
   }
