200e8c93936a278cb3ffafe29ea0d9e28084e34e
jxssx <danil.druzhina@gmail.com>
2022-10-19 12:56:40 +0300

    Fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,6 @@
 
 import readLineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
 const username = readLineSync.question('May i have your name? ');
 console.log(`Hello, ${username}!`);
