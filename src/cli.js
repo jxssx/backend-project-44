449d2889a95232cd164dd3a10099d70204325ffe
jxssx <danil.druzhina@gmail.com>
2022-10-20 18:51:05 +0300

    Fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -6,7 +6,7 @@ const user = {
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  user.name = readLineSync.question('May i have your name? ');
+  user.name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${user.name}!`);
 };
 
