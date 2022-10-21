779d1ec3449e7d8e88d6b076c440480e41b92409
jxssx <danil.druzhina@gmail.com>
2022-10-21 22:37:04 +0300

    Fix gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import game from '../src/index.js';
-import { desc, generateQuestion } from '../src/games/gcd.js';
+import runGame from '../src/games/gcd.js';
 
-game(desc, generateQuestion);
+runGame();
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,18 +1,23 @@
-#!/usr/bin/env node
+import { generateRandomNumber1to100 } from '../helpers.js';
+import game from '../index.js';
 
 const desc = 'Find the greatest common divisor of given numbers.';
 
-const generateQuestion = () => {
-  const firstNumber = Math.floor(Math.random() * 100);
-  const secondNumber = Math.floor(Math.random() * 100);
+const findGcd = (firstNumber, secondNumber) => {
   const leastNumber = Math.min(firstNumber, secondNumber);
   for (let i = 1; i <= leastNumber; i += 1) {
     const currentDevisor = leastNumber / i;
     if (firstNumber % currentDevisor === 0 && secondNumber % currentDevisor === 0) {
-      return [`${firstNumber} ${secondNumber}`, `${currentDevisor}`];
+      return currentDevisor;
     }
   }
-  return [`${firstNumber} ${secondNumber}`, '1'];
+  return 1;
 };
 
-export { desc, generateQuestion };
+const generateQuestion = () => {
+  const firstNumber = generateRandomNumber1to100();
+  const secondNumber = generateRandomNumber1to100();
+  return [`${firstNumber} ${secondNumber}`, `${findGcd(firstNumber, secondNumber)}`];
+};
+
+export default () => game(desc, generateQuestion);
