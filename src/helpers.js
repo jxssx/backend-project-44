a4c81a8bc516ccef238fbd05cb2eb335264c7760
jxssx <danil.druzhina@gmail.com>
2022-10-21 22:36:17 +0300

    Add helpers.js

---
+++ src/helpers.js
@@ -0,0 +1,5 @@
+const generateRandomNumber1to10 = () => Math.floor(Math.random() * 9) + 1;
+
+const generateRandomNumber1to100 = () => Math.floor(Math.random() * 99) + 1;
+
+export { generateRandomNumber1to10, generateRandomNumber1to100 };
